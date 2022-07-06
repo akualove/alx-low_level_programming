ca94dae10336e2456286ea88b98d721557b24cc8
akualove <106765044+akualove@users.noreply.github.com>
2022-07-06 18:44:27 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,16 @@
 #include "main.h"
+
+/**
+* _islower -> checks if the character is lowercase
+* @c: a charcter argument
+* Return: returns 1 and 0 depending on condition
+*/
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+
