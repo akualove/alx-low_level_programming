ae8543621d6786b4e2dcc63b011383fe4953ebe7
akualove <106765044+akualove@users.noreply.github.com>
2022-07-13 00:40:17 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,21 +4,22 @@
 
 /** 
  *main- genrate password
- *Return:nothing
+ *Return:0
  */
  
 int main(void)
 {
-int pass, sum;
+     int pass, sum;
 
-srand(time(NULL));
-sum = 0;
-while (sum <= 2645)
-{
-pass=(rand() % 128);
-sum += pass;
-printf(" %c", pass);
-}
-printf(" %c", 2772 - sum);
-return (0);
+     srand(time(NULL));
+     sum = 0;
+     while (sum <= 2645)
+     {
+           pass=(rand() % 128);
+           sum += pass;
+           printf("%c", pass);
+      }
+      printf("%c", 2772 - sum);
+
+      return (0);
 }
