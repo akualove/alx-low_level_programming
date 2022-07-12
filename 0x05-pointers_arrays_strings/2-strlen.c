9f4525905784914a11396517066029c9bb5c74d0
akualove <106765044+akualove@users.noreply.github.com>
2022-07-12 16:54:20 +0100

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,19 @@
-ill
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string parameter input
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int test;
+
+	for (test = 0; *s != '\0'; ++s)
+		++counter;
+
+	return (counter);
+}
